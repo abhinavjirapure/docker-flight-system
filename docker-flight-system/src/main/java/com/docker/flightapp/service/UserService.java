3071c646479d8fa22e5fab39b8f3d25c8811f70b
abhinav <abhinavrjirapure123.com>
2021-12-24 10:24:31 +0530

    commit

--- docker-flight-system/src/main/java/com/docker/flightapp/service/UserService.java
+++ docker-flight-system/src/main/java/com/docker/flightapp/service/UserService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import com.docker.flightapp.entity.UserEntity;
@@ -13,6 +14,7 @@ import com.docker.flightapp.repository.UserRepository;
 
 
 @Service
+@Component
 public class UserService {
 
 	private static final Logger logger = LoggerFactory.getLogger(UserService.class);
