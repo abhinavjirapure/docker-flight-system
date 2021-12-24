47a84c799260062a545edcc7c197f825a5dc655d
abhinav <abhinavrjirapure123.com>
2021-12-24 10:49:30 +0530

    scan

--- docker-flight-system/src/main/java/com/docker/flightapp/DockerFlightSystemApplication.java
+++ docker-flight-system/src/main/java/com/docker/flightapp/DockerFlightSystemApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({"com.docker.flightapp.repository","com.docker.flightapp.service"})
+@ComponentScan({"com.docker.flightapp.repository","com.docker.flightapp.service","com.docker.flightapp.entity"})
 public class DockerFlightSystemApplication {
 
 	public static void main(String[] args) {
